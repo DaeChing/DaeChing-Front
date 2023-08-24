59972370f322edd4a8f54e4e2bbc653cc9705e78
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-24 23:30:20 +0900

    [Chore] 회원가입 임시 뷰 생성

---
+++ DaeChing/DaeChing/View/SIgnIn/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  DaeChing
+//
+//  Created by Bokyung on 2023/08/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
