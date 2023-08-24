c8077954c88e1ebfe26adb7f0283fe2f0d1e9886
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-25 00:23:19 +0900

    [Feat] 로그인, 회원가입 모델 생성

--- DaeChing/DaeChing/Model/UserModel.swift
+++ DaeChing/DaeChing/Model/UserModel.swift
@@ -9,6 +9,15 @@ import SwiftUI
 
 // 업로드할 모델(형태) - 로그인
 struct UserModel: Codable {
+    let userName: String
     let userID: String
     let password: String
+}
+
+// 회원가입
+struct SignInModel: Codable {
+    let userName: String
+    let userID: String
+    let password: String
+    let status: String
 }
