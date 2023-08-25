6badbed5c3e9715d0e0f1a8fca367266da8b5a0d
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-25 21:02:51 +0900

    [Chore] 임시 파일 추가

---
+++ DaeChing/DaeChing/View/ManagePeople/ManagePeopleView.swift
@@ -0,0 +1,20 @@
+//
+//  ManagePeopleView.swift
+//  DaeChing
+//
+//  Created by Bokyung on 2023/08/25.
+//
+
+import SwiftUI
+
+struct ManagePeopleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ManagePeopleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ManagePeopleView()
+    }
+}
---
+++ DaeChing/DaeChing/View/ViewBuilder/ViewBuilders.swift
@@ -0,0 +1,20 @@
+//
+//  ViewBuilders.swift
+//  DaeChing
+//
+//  Created by Bokyung on 2023/08/25.
+//
+
+import SwiftUI
+
+struct ViewBuilders: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewBuilders_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewBuilders()
+    }
+}
