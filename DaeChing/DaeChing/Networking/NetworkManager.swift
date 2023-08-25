037e0ddca7bffd78199a9cf433fe6163d45f57bb
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-25 14:42:48 +0900

    [Feat] 지원서 통신 로직 작성

--- DaeChing/DaeChing/Networking/NetworkManager.swift
+++ DaeChing/DaeChing/Networking/NetworkManager.swift
@@ -269,9 +269,54 @@ func sendPostJobOffer(_ url: String, companyIntroduction: String, mainTasks: Str
 
 
 // MARK: 지원서 작성
-
-
-
+func sendPostApplyJob(_ url: String, introduction: String, portfolio: String, resume: String, awards: String, completion: @escaping ([String: Any]?, Error?) -> Void) {
+    guard let targetUrl = URL(string: url) else {
+        print("Error: cannot create URL")
+        return
+    }
+    
+    // 실제 업로드할 (데이터)인스턴스 생성
+    let uploadDataModel = ResumeModel(introduction: introduction, portfolio: portfolio, resume: resume, awards: awards)
+    
+    // 모델을 JSON data 형태로 변환
+    guard let jsonData = try? JSONEncoder().encode(uploadDataModel) else {
+        print("Error: Trying to convert model to JSON data")
+        return
+    }
+    
+   
+    // URL요청 생성
+    var request = URLRequest(url: targetUrl)
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type") // 요청타입 JSON
+    request.setValue("application/json", forHTTPHeaderField: "Accept") // 응답타입 JSON
+    request.httpBody = jsonData // 내가 원하는 데이터를 httpBody에 넣어서 보내줌
+    
+    
+    // 요청을 가지고 세션 작업시작
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        // 에러가 없어야 넘어감
+        guard error == nil else {
+            print("Error: error calling POST")
+            print(error!)
+            return
+        }
+        // 옵셔널 바인딩
+        guard let safeData = data else {
+            print("Error: Did not receive data")
+            return
+        }
+        // HTTP 200번대 정상코드인 경우만 다음 코드로 넘어감
+        guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
+            print("Error: HTTP request failed")
+            return
+        }
+        
+        // 원하는 모델이 있다면, JSONDecoder로 decode코드로 구현 ⭐️
+        print(String(decoding: safeData, as: UTF8.self))
+        
+    }.resume()   // 시작
+}
 
 
 
