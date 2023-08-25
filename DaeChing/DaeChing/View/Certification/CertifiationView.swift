81e45b9f87a19aa0bc48732fe7ded3f772a4ceeb
seoyeon-1206 <ssy_1206@naver.com>
2023-08-25 11:03:47 +0900

    구현 : MyState, MyPage, Certification View

--- DaeChing/DaeChing/View/Certification/CertifiationView.swift
+++ DaeChing/DaeChing/View/Certification/CertifiationView.swift
@@ -11,12 +11,168 @@ import SwiftUI
 struct CertifiationView: View {
     
     var body: some View {
-        Text("구직자의 내 이력 인증 View를 짜주시면 됩니당")
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("적성률")
+                        .font(.custom("Pretendard", size: 12).weight(.semibold))
+                    +
+                    // TODO: 컬러 고치기
+                    Text(" 60%")
+                        .foregroundColor(Color.blue)
+                        .font(.custom("Pretendard", size: 12).weight(.semibold))
+                    //                    Divider()
+                    //                        .frame(height: 16)
+                    Rectangle()
+                        .frame(width: 0.5,height: 12)
+                    Text("내 이력을 인증하고 지원해 보세요")
+                        .font(.custom("Pretendard", size: 12).weight(.semibold))
+                }
+                .padding(.leading, 12)
+                
+                Rectangle()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 1)
+                    .foregroundColor(.blue)
+                    .padding(.bottom,12)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("경력")
+                            .font(.custom("Pretendard", size: 20).weight(.bold))
+                        Spacer()
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .padding(.bottom, 10)
+                    Text("총 경력")
+                        .font(.custom("Pretendard", size: 12).weight(.bold))
+                    + Text(" 1년 1개월")
+                        .font(.custom("Pretendard", size: 12).weight(.bold))
+                        .foregroundColor(.gray)
+                    
+                    Rectangle()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 1)
+                        .foregroundColor(.gray)
+                        .padding(.bottom,12)
+                    
+                    Text("PINEAPPLE")
+                        .font(.custom("Pretendard", size: 16).weight(.bold))
+                    HStack {
+                        Text("iOS 개발자")
+                            .font(.custom("Pretendard", size: 16).weight(.medium))
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }
+                    .padding(.vertical, 10)
+                    Text("2022.03 - 2023. 04")
+                    
+                }
+                .padding(.horizontal,12)
+                Rectangle()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 1)
+                    .foregroundColor(.gray)
+                    .padding(.vertical,12)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("학력")
+                            .font(.custom("Pretendard", size: 20).weight(.bold))
+                        Spacer()
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    Text("학교명")
+                        .font(.custom("Pretendard", size: 16).weight(.bold))
+                        .padding(.vertical, 8)
+                    HStack {
+                        Text("전공 및 학위")
+                            .font(.custom("Pretendard", size: 16).weight(.medium))
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }
+                    .padding(.bottom, 8)
+                    Text("년.월 - 년.월")
+                        .font(.custom("Pretendard", size: 16).weight(.medium))
+                    
+                }
+                .padding(.horizontal,12)
+                Rectangle()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 1)
+                    .foregroundColor(.gray)
+                    .padding(.vertical,12)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("활동 및 수상")
+                            .font(.custom("Pretendard", size: 20).weight(.bold))
+                        Spacer()
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    HStack {
+                        Text("대외활동, 봉사활동, 수상내역 등")
+                            .font(.custom("Pretendard", size: 16).weight(.bold))
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }
+                    .padding(.vertical, 8)
+                    Text("년.월 - 년.월")
+                        .font(.custom("Pretendard", size: 16).weight(.medium))
+                    
+                }
+                .padding(.horizontal,14)
+                .navigationBarTitle("내 이력 인증",displayMode: .inline)
+                
+                HStack {
+                    Spacer()
+                    BtnComponet(btnColor: Color.gray, btnName: "임시저장", textColor: .black, btnWidth: 162, btnHeight: 52, btnRadius: 12, action: {
+                        print("눌렸다")})
+                    
+                    BtnComponet(btnColor: Color.blue, btnName: "작성완료", textColor: .white, btnWidth: 162, btnHeight: 52, btnRadius: 12, action: {
+                        print("또 눌렸다.")
+                    })
+                    Spacer()
+                }
+                
+            }
+            
+        }
+        
+        
+        
     }
 }
+
 
 struct CertifiationView_Previews: PreviewProvider {
     static var previews: some View {
         CertifiationView()
+    }
+}
+
+struct BtnComponet: View {
+    var btnColor: Color
+    var btnName: String
+    var textColor: Color
+    var btnWidth: CGFloat
+    var btnHeight: CGFloat
+    var btnRadius: CGFloat
+    var action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            RoundedRectangle(cornerRadius: btnRadius)
+                .frame(width: btnWidth,height: btnHeight)
+                .foregroundColor(btnColor)
+                .overlay {
+                    Text(btnName)
+                        .foregroundColor(textColor)
+                        .font(.system(size: 16,weight: .bold))
+                }
+        }
+        
     }
 }
--- DaeChing/DaeChing/View/MyPage/MyPageView.swift
+++ DaeChing/DaeChing/View/MyPage/MyPageView.swift
@@ -9,10 +9,64 @@ import SwiftUI
 
 // MARK: 신서연의 View❤️
 struct MyPageView: View {
+    let imageName = [["person", "alarm", "email"],["resume", "handShake", "save"], ["pencil", "follow", "q&a"] ]
+    
+    let labelText = [["계정", "알람 설정", "이메일 수신 설정"], ["내 이력서", "입사 제안", "저장한 채용 공고"], ["리뷰 작성 내역", "팔로잉 기업", "질문 내역"]]
+    
     var body: some View {
-        Text("구직자의 내 정보 뷰를 짜주시면 됩니당!")
+        NavigationView {
+            VStack (alignment: .leading){
+                Text("MY")
+                    .font(.custom("Pretendard", size: 20).weight(.bold))
+                    .padding(20)
+                ForEach(imageName.indices, id: \.self) { index in
+                    iconTextView(imageName: imageName[0][index]
+                                 , labelText: labelText[0][index])
+                }
+                Text("채용")
+                    .padding(20)
+                    .font(.custom("Pretendard", size: 20).weight(.bold))
+                ForEach(imageName.indices, id: \.self) { index in
+                    iconTextView(imageName: imageName[1][index]
+                                 , labelText: labelText[1][index])
+                }
+                Text("활동")
+                    .font(.custom("Pretendard", size: 20).weight(.bold))
+                    .padding(20)
+                ForEach(imageName.indices, id: \.self) { index in
+                    iconTextView(imageName: imageName[2][index]
+                                 , labelText: labelText[2][index])
+                }
+                Spacer()
+            }.navigationBarTitle("내 정보", displayMode: .inline)
+                .navigationBarItems(trailing: Image(systemName: "bell").padding(.trailing,20))
+                
+            
+               
+            
+                
+        }
     }
 }
+
+struct iconTextView: View {
+    var imageName: String
+    var labelText: String
+    var body: some View {
+        VStack{
+            HStack(spacing: 20) {
+                Image(imageName)
+                Text(labelText)
+                    .font(.custom("Pretendard", size: 20).weight(.bold))
+                Spacer()
+            }.padding(.leading)
+            
+        }
+    }
+        
+    }
+
+
 
 struct MyPageView_Previews: PreviewProvider {
     static var previews: some View {
