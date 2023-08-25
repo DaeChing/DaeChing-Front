dfea4b94bea8d793539ae39e42a24dd29e2d09b2
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-25 10:48:22 +0900

    [Feat] 공고 올리기뷰 구현 및 POST 메소드 작성

--- DaeChing/DaeChing/Networking/NetworkManager.swift
+++ DaeChing/DaeChing/Networking/NetworkManager.swift
@@ -212,12 +212,61 @@ func sendPostRequestSignIn(_ url: String, userName: String, userID: String, pass
 }
 
 
+// 공고 올리기
+func sendPostJobOffer(_ url: String, companyIntroduction: String, mainTasks: String, qualificationRequirements: String, preferredQualifications: String, recruitmentProcess: String, welfare: String, completion: @escaping ([String: Any]?, Error?) -> Void) {
+    guard let targetUrl = URL(string: url) else {
+        print("Error: cannot create URL")
+        return
+    }
+    
+    // 실제 업로드할 (데이터)인스턴스 생성
+    let uploadDataModel = CompanyModel(companyIntroduction: "기업소개", mainTasks: "주요업무", qualificationRequirements: "자격요건", preferredQualifications: "우대조건", recruitmentProcess: "채용절차", welfare: "복리후생")
+    
+    // 모델을 JSON data 형태로 변환
+    guard let jsonData = try? JSONEncoder().encode(uploadDataModel) else {
+        print("Error: Trying to convert model to JSON data")
+        return
+    }
+    
+   
+    // URL요청 생성
+    var request = URLRequest(url: targetUrl)
+    request.httpMethod = "POST"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type") // 요청타입 JSON
+    request.setValue("application/json", forHTTPHeaderField: "Accept") // 응답타입 JSON
+    request.httpBody = jsonData // 내가 원하는 데이터를 httpBody에 넣어서 보내줌
+    
+    
+    // 요청을 가지고 세션 작업시작
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        // 에러가 없어야 넘어감
+        guard error == nil else {
+            print("Error: error calling POST")
+            print(error!)
+            return
+        }
+        // 옵셔널 바인딩
+        guard let safeData = data else {
+            print("Error: Did not receive data")
+            return
+        }
+        // HTTP 200번대 정상코드인 경우만 다음 코드로 넘어감
+        guard let response = response as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
+            print("Error: HTTP request failed")
+            return
+        }
+        
+        // 원하는 모델이 있다면, JSONDecoder로 decode코드로 구현 ⭐️
+        print(String(decoding: safeData, as: UTF8.self))
+        
+    }.resume()   // 시작
+}
+
 
 // postMethod()
 
 // MARK: PUT 서버에 현존하는 데이터 업데이트하기
 // 나의 포스트 수정, 다른 사람의 게시글 좋아요 누르기, 나의 정보 수정
-
 func putMethod() {
     guard let url = URL(string: "https://reqres.in/api/users/2") else {
         print("Error: cannot create URL")
---
+++ DaeChing/DaeChing/View/Main/UploadJobOfferView.swift
@@ -0,0 +1,171 @@
+//
+//  UploadJobOfferView.swift
+//  DaeChing
+//
+//  Created by Bokyung on 2023/08/25.
+//
+
+import SwiftUI
+
+struct UploadJobOfferView: View {
+    @State var companyIntroduction: String = "" // 기업소개
+    @State var mainTasks: String = "" // 주요업무
+    @State var qualificationRequirements: String = "" // 자격요건
+    @State var preferredQualifications: String = "" // 우대사항
+    @State var recruitmentProcess: String = "" // 채용절차
+    @State var welfare: String = "" // 복리후생
+    
+    @Environment(\.presentationMode) var presentationMode // 현재의 뷰를 제거(뒤로 가기)하기 위한 변수
+    
+    @State var isUpload: Bool = false
+    
+    var body: some View {
+        ScrollView {
+            VStack (alignment: .leading, spacing: 0){
+                Text("(주)토피아")
+                    .font(.pretendard(.black, size: 24))
+                    .foregroundColor(.systemBlack)
+                Text("iOS Developer | 대구 북구")
+                    .font(.pretendard(.medium, size: 15))
+                    .foregroundColor(.systemGray1)
+                
+                Image("ImgJootpia")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 24)
+                
+                VStack (spacing: 25){
+                    VStack (alignment: .leading,spacing: 11){
+                        Text("기업소개")
+                            .font(.pretendard(.bold, size: 16))
+                            .foregroundColor(.systemBlack)
+                        TextEditor(text: $companyIntroduction)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 164)
+                            .background(Color.clear)
+                            .overlay(content: {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .inset(by: 0.5)
+                                    .stroke(.blue)
+                            })
+                    } // 기업소개
+                    
+                    VStack (alignment: .leading,spacing: 11){
+                        Text("주요 업무")
+                            .font(.pretendard(.bold, size: 16))
+                            .foregroundColor(.systemBlack)
+                        TextEditor(text: $mainTasks)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 164)
+                            .background(Color.clear)
+                            .overlay(content: {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .inset(by: 0.5)
+                                    .stroke(.blue)
+                            })
+                    } // 주요 업무
+                    
+                    VStack (alignment: .leading,spacing: 11){
+                        Text("자격 요건")
+                            .font(.pretendard(.bold, size: 16))
+                            .foregroundColor(.systemBlack)
+                        TextEditor(text: $qualificationRequirements)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 164)
+                            .background(Color.clear)
+                            .overlay(content: {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .inset(by: 0.5)
+                                    .stroke(.blue)
+                            })
+                    } // 자격 요건
+                    
+                    
+                    VStack (alignment: .leading,spacing: 11){
+                        Text("우대사항")
+                            .font(.pretendard(.bold, size: 16))
+                            .foregroundColor(.systemBlack)
+                        TextEditor(text: $preferredQualifications)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 164)
+                            .background(Color.clear)
+                            .overlay(content: {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .inset(by: 0.5)
+                                    .stroke(.blue)
+                            })
+                    } // 우대사항
+                    
+                    VStack (alignment: .leading,spacing: 11){
+                        Text("채용절차")
+                            .font(.pretendard(.bold, size: 16))
+                            .foregroundColor(.systemBlack)
+                        TextEditor(text: $recruitmentProcess)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 164)
+                            .background(Color.clear)
+                            .overlay(content: {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .inset(by: 0.5)
+                                    .stroke(.blue)
+                            })
+                    } // 채용절차
+                    
+                    VStack (alignment: .leading,spacing: 11){
+                        Text("복리후생")
+                            .font(.pretendard(.bold, size: 16))
+                            .foregroundColor(.systemBlack)
+                        TextEditor(text: $welfare)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 164)
+                            .background(Color.clear)
+                            .overlay(content: {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .inset(by: 0.5)
+                                    .stroke(.blue)
+                            })
+                    } // 복리후생
+                } // TextFields
+                .padding(.bottom, 25)
+                
+                
+                Button {
+                    // MARK: 업로드 - 공고 올리기
+                    sendPostJobOffer("url", companyIntroduction: companyIntroduction, mainTasks: mainTasks, qualificationRequirements: qualificationRequirements, preferredQualifications: preferredQualifications, recruitmentProcess: recruitmentProcess, welfare: welfare) { responseObject, error in guard let _ = responseObject, error == nil else {
+                        print(error ?? "Unknown error")
+                        return
+                    }
+                    }
+                    presentationMode.wrappedValue.dismiss() // 현재의 뷰를 제거(뒤로 가기)하기 위한 변수
+                    isUpload = true
+                } label: {
+                    Text("등록하기")
+                        .font(.pretendard(.bold, size: 16))
+                        .foregroundColor(.white)
+                        .padding(.vertical, 15)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.mainPrimary500.cornerRadius(12))
+                }
+                
+                
+                
+            } // VStack
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 25)
+        } // ScrollView
+        .alert(isPresented: $isUpload) {
+            Alert(title: Text("정상적으로 업로드 완료하였습니다!"), message: Text("열심히 작성하신 구인공고가 정상적으로 업로드되었습니다"), dismissButton: .default(Text("확인"), action: {
+                presentationMode.wrappedValue.dismiss() // 현재의 뷰를 제거(뒤로 가기)하기 위한 변수
+            })) // Alert
+        } // alert
+        .navigationTitle("공고 올리기")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct UploadJobOfferView_Previews: PreviewProvider {
+    static var previews: some View {
+        UploadJobOfferView()
+    }
+}
